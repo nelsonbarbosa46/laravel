a12927bf7b41dfb13b0135741f49cdd2be76cdd2
Nelson Barbosa <nelson.fb24@gmail.com>
2021-05-08 18:33:13 +0100

    Inserindo valores no view

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -8,6 +8,22 @@ class ProductsController extends Controller
 {
     //
     public function index() {
-        return view('products.index');
+        $product = "apple";
+
+        $products = [
+            'p1' => "orange",
+            'p2' => 'pear',
+        ];
+        // Usando "compact"
+        // return view('products.index', compact('product'));
+        
+        //Usando "With"
+        // return view('products.index')->with('product', $product));
+        // return view('products.index')->with('products', $products);
+
+        //Inserindo diretamente
+        return view('products.index', [
+            'products' => $products
+        ]);
     }
 }
